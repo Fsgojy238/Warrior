0b77ed3d82f4cdb366e72e3396985c2b00543614
wei wen Chen <2565057553@qq.com>
2025-08-14 18:32:40 +0800

    完成 Hit React Sound FX

--- Source/Warrior/Private/WarriorFunctionLibrary.cpp
+++ Source/Warrior/Private/WarriorFunctionLibrary.cpp
@@ -7,6 +7,7 @@
 #include "Interface/PawnCombatInterface.h"
 #include "GenericTeamAgentInterface.h"
 #include "Kismet/KismetMathLibrary.h"
+#include "WarriorGameplayTags.h"
 
 UWarriorAbilitySystemComponent* UWarriorFunctionLibrary::NativeGetWarriorASCFromActor(AActor* InActor)
 {
@@ -92,10 +93,14 @@ FGameplayTag UWarriorFunctionLibrary::ComputeHitReacDirectionTag(AActor* InAttac
 {
 	check(InAttacker && InVictim);
 
+	//受害者正前方
 	const FVector VictimForward = InVictim->GetActorForwardVector();
+	//从受害者位置到攻击者位置的向量
 	const FVector VictimToAttackerNormalized = (InAttacker->GetActorLocation() - InVictim->GetActorLocation()).GetSafeNormal();
 
+	//点积计算角度差，给机器看的
 	const float DotResult = FVector::DotProduct(VictimForward, VictimToAttackerNormalized);
+	//把点积结果换成人能看懂的实际角度（比如0度~180度）
 	OutAngleDifference = UKismetMathLibrary::DegAcos(DotResult);
 
 	const FVector CrossResult = FVector::CrossProduct(VictimForward, VictimToAttackerNormalized);
@@ -105,5 +110,21 @@ FGameplayTag UWarriorFunctionLibrary::ComputeHitReacDirectionTag(AActor* InAttac
 		OutAngleDifference *= -1.f;
 	}
 
-	return FGameplayTag();
+	if (OutAngleDifference <= 45.f && OutAngleDifference >= -45.f)
+	{
+		return WarriorGameplayTags::Shared_Status_HitReact_Front;
+	}
+	else if (OutAngleDifference > 45.f && OutAngleDifference < 135.f)
+	{
+		return WarriorGameplayTags::Shared_Status_HitReact_Right;
+	}
+	else if (OutAngleDifference >= 135.f || OutAngleDifference <= -135.f)
+	{
+		return WarriorGameplayTags::Shared_Status_HitReact_Back;
+	}
+	else if (OutAngleDifference > -135.f && OutAngleDifference < -45.f)
+	{
+		return WarriorGameplayTags::Shared_Status_HitReact_Left;
+	}
+	return WarriorGameplayTags::Shared_Status_HitReact_Front;
 }
