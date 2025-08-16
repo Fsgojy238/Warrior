6c438a160adf2d0af814ce8edea9eab4c18b5aae
wei wen Chen <2565057553@qq.com>
2025-08-17 00:01:00 +0800

    完成 Get Available Targets

--- Intermediate/Build/Win64/UnrealEditor/Inc/Warrior/UHT/Warrior.init.gen.cpp
+++ Intermediate/Build/Win64/UnrealEditor/Inc/Warrior/UHT/Warrior.init.gen.cpp
@@ -23,13 +23,13 @@ void EmptyLinkFunctionForGeneratedCodeWarrior_init() {}
 				SingletonFuncArray,
 				UE_ARRAY_COUNT(SingletonFuncArray),
 				PKG_CompiledIn | 0x00000000,
-				0x333E35C5,
-				0x0BD7FC54,
+				0xCCF83BB1,
+				0x7338F274,
 				METADATA_PARAMS(0, nullptr)
 			};
 			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_Warrior.OuterSingleton, PackageParams);
 		}
 		return Z_Registration_Info_UPackage__Script_Warrior.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_Warrior(Z_Construct_UPackage__Script_Warrior, TEXT("/Script/Warrior"), Z_Registration_Info_UPackage__Script_Warrior, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x333E35C5, 0x0BD7FC54));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_Warrior(Z_Construct_UPackage__Script_Warrior, TEXT("/Script/Warrior"), Z_Registration_Info_UPackage__Script_Warrior, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xCCF83BB1, 0x7338F274));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
