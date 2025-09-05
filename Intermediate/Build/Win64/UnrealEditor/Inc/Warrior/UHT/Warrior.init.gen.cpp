68d73ac61a450e407a55afa18ed19f708a7565e1
wei wen Chen <2565057553@qq.com>
2025-09-05 12:06:12 +0800

    完成 BP_Stone

--- Intermediate/Build/Win64/UnrealEditor/Inc/Warrior/UHT/Warrior.init.gen.cpp
+++ Intermediate/Build/Win64/UnrealEditor/Inc/Warrior/UHT/Warrior.init.gen.cpp
@@ -31,13 +31,13 @@ void EmptyLinkFunctionForGeneratedCodeWarrior_init() {}
 				SingletonFuncArray,
 				UE_ARRAY_COUNT(SingletonFuncArray),
 				PKG_CompiledIn | 0x00000000,
-				0xAA4A4B65,
-				0x697334C9,
+				0x907B90A6,
+				0x09CFD7C9,
 				METADATA_PARAMS(0, nullptr)
 			};
 			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_Warrior.OuterSingleton, PackageParams);
 		}
 		return Z_Registration_Info_UPackage__Script_Warrior.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_Warrior(Z_Construct_UPackage__Script_Warrior, TEXT("/Script/Warrior"), Z_Registration_Info_UPackage__Script_Warrior, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xAA4A4B65, 0x697334C9));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_Warrior(Z_Construct_UPackage__Script_Warrior, TEXT("/Script/Warrior"), Z_Registration_Info_UPackage__Script_Warrior, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x907B90A6, 0x09CFD7C9));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ Source/Warrior/Private/Items/PickUps/WarriorStoneBase.cpp
@@ -0,0 +1,5 @@
+// Eric Chen 
+
+
+#include "Items/PickUps/WarriorStoneBase.h"
+
